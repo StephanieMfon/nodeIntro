6fb5b883c06b68158afaf5a490ffceab12fc80b1
StephanieMfon <108840244+StephanieMfon@users.noreply.github.com>
2023-03-29 01:41:27 +0100

    modified readme and updated js file

---
+++ index.js
@@ -0,0 +1,8 @@
+const fullName = "My name is Stephanie Crispin Okpo-mfon";
+const prefferedLanguage = "My preffered language currently is Javascript";
+const meInOneYear =
+  "I see myself working as a full stack developer for Paypal, while venturing into machine learning.";
+
+console.log(fullName);
+console.log(prefferedLanguage);
+console.log(meInOneYear);
